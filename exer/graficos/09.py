b5cb0124b1dca595fb0f528cbe2e56a2b72156da
João Hugo Leda <joaohugoleda@gmail.com>
2021-05-14 20:18:47 -0300

    14--05

---
+++ exer/graficos/09.py
@@ -0,0 +1,27 @@
+# Visualização de dados crescimento da populção brasileira 1980-2016
+
+import matplotlib.pyplot as plt
+
+dados = open("populacao.csv").readlines()
+
+x, y = []
+
+for i in range(len(dados)):
+  if i != 0:
+    linha = dados[i].split(";")
+    x.append(int(linha[0]))
+    y.append(int(linha[1]))
+
+titulo = "Populção brasileira 1980-2016"
+eixox = "Ano"
+eixoy = "População brasileira (x⁷)"
+
+plt.bar(x, y, color = "#ccc")
+plt.plot(x, y, color = 'k', linestyle = "--")
+
+plt.title(titulo)
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
+
+plt.savefig("09.png", dpi = 600)
+plt.show()
