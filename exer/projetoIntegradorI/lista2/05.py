808953c609a60cc165bacbc13cdeb8a79bb4b3b8
João Hugo Leda <joaohugoleda@gmail.com>
2021-08-03 20:48:47 -0300

    06

--- exer/projetoIntegradorI/lista2/05.py
+++ exer/projetoIntegradorI/lista2/05.py
@@ -1,1 +1,10 @@
 # 5) Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.
+
+n = float(input('Informe um número: '))
+
+if (n > 0):
+  res = n * 2
+else:
+  res = n * 3
+
+print(f'Resultado: {res}')
--- exer/projetoIntegradorI/lista2/06.py
+++ exer/projetoIntegradorI/lista2/06.py
@@ -1,1 +1,13 @@
 # 6) Faça um programa em Python que leia a média de duas avaliações (P1 e P2). Calcule e mostre a média final do aluno. Calcule e mostre se o aluno foi APROVADO ou REPROVADO, sabendo que a média para aprovação deve ser maior ou igual a 7.
+
+n1 = float(input('Informe a nota da P1 do aluno: '))
+n2 = float(input('Informe a nota da P2 do aluno: '))
+
+media = (n1 + n2) / 2
+
+if (media >= 7):
+  res = 'APROVADO'
+else:
+  res = 'REPROVADO'
+
+print('O aluno foi:', res)
