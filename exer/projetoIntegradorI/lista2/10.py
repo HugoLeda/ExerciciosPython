59a194e009cdc0857c4fc75c9cdd615ba442c65d
João Hugo Leda <joaohugoleda@gmail.com>
2021-08-03 22:44:55 -0300

    11

--- exer/projetoIntegradorI/lista2/10.py
+++ exer/projetoIntegradorI/lista2/10.py
@@ -7,3 +7,18 @@
 #Entre 18,5 e 25 Peso normal
 #Entre 25 e 30 Acima do peso
 #Acima de 30 obeso
+
+peso = float(input('Informe o peso em Kg: '))
+altura = float(input('Informe a altura em metros: '))
+imc = peso / (altura * altura)
+
+if (imc < 18.5):
+  res = 'Abaixo do peso'
+elif (imc >= 18.5 and imc < 25):
+  res = 'Peso normal'
+elif (imc >= 25 and imc < 30):
+  res = 'Acima do peso'
+else:
+  res = 'Obeso'
+
+print('IMC:', imc)
--- exer/projetoIntegradorI/lista2/11.py
+++ exer/projetoIntegradorI/lista2/11.py
@@ -5,4 +5,20 @@
 #1 À vista em dinheiro ou cheque, recebe 10% de desconto
 #2 À vista no cartão de crédito, recebe 15% de desconto
 #3 Em duas vezes, preço normal de etiqueta sem juros
-#4 Em duas vezes, preço normal de etiqueta mais juros de 10%
+#4 Em três vezes, preço normal de etiqueta mais juros de 10%
+
+preco = float(input('Informe o preço do produto: '))
+pag = int(input('Informe o código de pagamento: '))
+
+if (pag == 1):
+  preco -= preco * 0.1
+elif (pag == 2):
+  preco -= (preco * 0.15)
+elif (pag == 3):
+  preco = preco
+elif (pag == 4):
+  preco += preco * 0.1
+else:
+  print('Código de pagamento inválido!')
+
+print(f' Valor a ser pago: {round(preco, 2)}')
