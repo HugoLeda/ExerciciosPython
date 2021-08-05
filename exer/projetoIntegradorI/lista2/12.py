2c6962bfdf9953c59d6dd6c347d7aac1322a9aaf
João Hugo Leda <joaohugoleda@gmail.com>
2021-08-04 22:56:51 -0300

    12

--- exer/projetoIntegradorI/lista2/12.py
+++ exer/projetoIntegradorI/lista2/12.py
@@ -6,8 +6,34 @@
 
 #Média de aproveitamento Conceito
 
-#>= 90 A
-#>= 75 e < 90 B
-#>= 60 e < 75 C
-#>= 40 e < 60 D
-#< 40 E
+#>= 9.0 A
+#>= 7.5 e < 9.0 B
+#>= 6.0 e < 7.5 C
+#>= 4.0 e < 6.0 D
+#< 4.0 E
+
+ni = int(input('Informe o número de identificação: '))
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+n3 = float(input('Digite a nota 3: '))
+me = float(input('Informe a média dos exercícios: '))
+
+ma = (n1 + (n2 * 2) + (n3 * 3) + me) / 7
+
+if (ma >= 9):
+  conceito = 'A'
+  res = 'Aprovado'
+elif (ma >= 7.5 and ma < 9):
+  conceito = 'B'
+  res = 'Aprovado'
+elif (ma >= 6 and ma < 7.5):
+  conceito = 'C'
+  res = 'Aprovado'
+elif (ma >= 4 and ma < 6):
+  conceito = 'D'
+  res = 'Reprovado'
+else:
+  conceito = 'E'
+  res = 'Reprovado'
+
+print(f'Número do aluno: {ni}\nNota 1: {n1}\nNota 2: {n2}\nNota 3: {n3}\nMédia dos exercícios: {round(me, 2)}\nMédia de aproveitamento: {round(ma, 2)}\nConceito: {conceito}\nResultado: {res}')
