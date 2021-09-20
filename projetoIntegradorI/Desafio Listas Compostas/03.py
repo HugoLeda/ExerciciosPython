2f84a02c1ec494fb676e78a739c16c77e11bdb07
João Hugo Leda <joaohugoleda@gmail.com>
2021-09-19 21:04:28 -0300

    02

--- projetoIntegradorI/Desafio Listas Compostas/03.py
+++ projetoIntegradorI/Desafio Listas Compostas/03.py
@@ -1,1 +1,12 @@
 # Faça um script que leia uma matriz 2 X 2 de números inteiros. Mostre no final a matriz em formatação correta (forma de matriz).
+
+matriz = [[], []]
+
+for i in range(2):
+  for j in range(2):
+    n = int(input(f'Digite o elemento {i + 1}{j + 1}: '))
+    matriz[i].append(n)
+
+print('Matriz 2x2: ')
+for i in range(len(matriz)):
+  print(matriz[i])
