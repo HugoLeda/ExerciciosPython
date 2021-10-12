ebcc89e6231c8f6937269be3b7046b2704de2fae
João Hugo Leda <joaohugoleda@gmail.com>
2021-10-12 09:43:30 -0300

    01 funcoes

--- projetoIntegradorI/Desafio funcoes pt 1/01.py
+++ projetoIntegradorI/Desafio funcoes pt 1/01.py
@@ -1,1 +1,14 @@
 # .Faça uma função que receba n valores (usando o desempacotador *) e exiba a soma dos valores.
+
+from random import randint
+
+def soma(valores: list):
+  x, *restante = valores
+  soma = x
+  
+  for i in range(len(restante)):
+    soma += restante[i]
+
+  return soma
+
+print(soma([randint(-100, 100), randint(-100, 100), randint(-100, 100), randint(-100, 100)]))
