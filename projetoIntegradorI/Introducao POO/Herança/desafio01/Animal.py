f5f84ddb472b74fc2597b20e54036e69e7bcb127
João Hugo Leda <joaohugoleda2@gmail.com>
2021-11-13 22:50:55 -0300

    arquivos.

--- projetoIntegradorI/Introducao POO/Herança/desafio01/Animal.py
+++ projetoIntegradorI/Introducao POO/Herança/desafio01/Animal.py
@@ -1,2 +1,16 @@
 class Animal:
-  
+  def __init__(self, nome: str, sexo: str):
+    self.nome = nome
+    self.sexo = sexo
+
+  def exibirDados(self):
+    print(f'Nome: {self.nome}\nSexo: {self.sexo}')
+
+  def cacar(self):
+    print(f'{self.nome} caçou!')
+
+  def comer(self):
+    print(f'{self.nome} comeu!')
+
+  def dormir(self):
+    print(f'{self.nome} dormiu!')
