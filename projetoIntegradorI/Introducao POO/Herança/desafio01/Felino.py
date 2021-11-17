eb0e4db3b7aa1ddadb6f8b9b0918c2e4c8950ec8
João Hugo Leda <joaohugoleda2@gmail.com>
2021-11-16 23:08:37 -0300

    heranca

--- projetoIntegradorI/Introducao POO/Herança/desafio01/Felino.py
+++ projetoIntegradorI/Introducao POO/Herança/desafio01/Felino.py
@@ -1,4 +1,9 @@
 from Animal import Animal
 
 class Felino(Animal):
+  def __init__(self, nome: str, sexo: str, garras: bool):
+    super().__init__(nome, sexo)
+    self.garrasRetrateis = garras
   
+  def ronronar(self):
+    print(f'{self.nome} ronronou!')
